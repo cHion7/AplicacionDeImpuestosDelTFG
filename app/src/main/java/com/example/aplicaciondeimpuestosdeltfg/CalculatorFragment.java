5fdb4f9c619c5a36e9622ecf45a15a6ef194f22a
Hugo osma castrosin <perdiceman@gmail.com>
2025-04-07 22:52:50 +0200

    Creado taop view y menu en el CalculatorFragment

---
+++ app/src/main/java/com/example/aplicaciondeimpuestosdeltfg/CalculatorFragment.java
@@ -0,0 +1,35 @@
+package com.example.aplicaciondeimpuestosdeltfg;
+
+import android.os.Bundle;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class CalculatorFragment extends Fragment {
+
+    private DrawerLayout drawerLayout;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflar la vista del fragmento
+        View view = inflater.inflate(R.layout.fragment_calculator, container, false);
+
+        // Referencia al DrawerLayout (desde el layout del fragmento)
+        drawerLayout = view.findViewById(R.id.drawer_layout);
+
+        // Configurar el botón (ImageView) para abrir el menú lateral
+        ImageView botonAbrirMenu = view.findViewById(R.id.btn_open_menu);
+        botonAbrirMenu.setOnClickListener(v -> {
+            if (drawerLayout != null && !drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/aplicaciondeimpuestosdeltfg/MainActivity.java
+++ app/src/main/java/com/example/aplicaciondeimpuestosdeltfg/MainActivity.java
@@ -1,24 +1,56 @@
 package com.example.aplicaciondeimpuestosdeltfg;
 
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+   BottomNavigationView bottomNavigationView;
+   HomeFragment homeFragment = new HomeFragment();
+   AjustesFragment settings = new AjustesFragment();
+   CalculatorFragment calculatorFragment = new CalculatorFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_main); // Asegúrate que este layout tenga el BottomNavigationView y el FrameLayout con los IDs correctos
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,homeFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.home) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.frame_container, homeFragment)
+                            .commit();
+                    return true;
+                } else if (id == R.id.ajustes) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.frame_container, settings)
+                            .commit();
+                    return true;
+                } else if (id == R.id.calculadora) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.frame_container, calculatorFragment)
+                            .commit();
+                    return true;
+                }
+
+                return false;
+            }
+
         });
     }
 }
