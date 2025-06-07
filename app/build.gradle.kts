967c5506455cb21c152331fd656aef5ced692d15
Huihui <huiz0517@gmail.com>
2025-06-07 03:38:30 +0200

    Foto de perfil hecho

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,10 @@ dependencies {
     implementation("com.github.prolificinteractive:material-calendarview:2.0.0")
 
     implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
     //Iniciar sesión con Google
     implementation ("com.google.android.gms:play-services-auth:20.6.0")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
--- app/src/main/java/com/example/aplicaciondeimpuestosdeltfg/vistas/PerfilFragment.java
+++ app/src/main/java/com/example/aplicaciondeimpuestosdeltfg/vistas/PerfilFragment.java
@@ -2,18 +2,31 @@ package com.example.aplicaciondeimpuestosdeltfg.vistas;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import android.provider.MediaStore;
 import android.text.InputType;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -33,16 +46,88 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.ByteArrayOutputStream;
+
 
 public class PerfilFragment extends Fragment {
+    // Constantes para manejo de permisos y cámara
+    private static final int REQUEST_PERMISSION = 1;
+    private static final int CAMERA_REQUEST_CODE = 100;
+
     private TextInputEditText etNombreApellidoPerfil, etUsuarioPerfil, etSaldoInicialPerfil;
-    private Button btCambiarContrasenaPerfil, btCerrarSesion, btAnadriImpuestosPerfil, btModificarDatos;
+    private Button btCambiarContrasenaPerfil, btAnadriImpuestosPerfil, btModificarDatos;
     private TextView btVerTodoPerfil;
+    private ImageView ivFotoPerfil;
     private ImageButton btCerrarSesionPerfil;
+
     private FirebaseAuth firebaseAuth;
     private FirebaseDatabase database;
     private DatabaseReference nodoUsuario;
     private FirebaseUser usuarioActual;
+    private ActivityResultLauncher<Intent> cameraLauncher;
+
+    // Parámetros opcionales para el fragmento
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public PerfilFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment PerfilFragmento.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static PerfilFragment newInstance(String param1, String param2) {
+        PerfilFragment fragment = new PerfilFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+        //Configurar el launcher para la cámara
+        cameraLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == getActivity().RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null && data.getExtras() != null) {
+                            Bitmap photo = (Bitmap) data.getExtras().get("data");
+                            if (photo != null) {
+                                ivFotoPerfil.setImageBitmap(photo);
+                                subirImagenAFirebase(photo);
+                            } else {
+                                Toast.makeText(getActivity(), "No se ha capturado ninguna imagen", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Log.e("PerfilFragment", "Intent data es null");
+                        }
+                    } else {
+                        Log.e("PerfilFragment", "Error: resultCode incorrecto");
+                    }
+                });
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -61,11 +146,12 @@ public class PerfilFragment extends Fragment {
         etUsuarioPerfil.setEnabled(false);
         btVerTodoPerfil = view.findViewById(R.id.tvVerTodoDatosPerfil);
         etSaldoInicialPerfil = view.findViewById(R.id.etSaldoInicialPerf);
+        ivFotoPerfil = view.findViewById(R.id.ivPerfil);
+        ivFotoPerfil.setImageResource(R.drawable.perfilimpuestos3);
         btModificarDatos = view.findViewById(R.id.btModificarDatosPerfil);
         btCambiarContrasenaPerfil = view.findViewById(R.id.btCambiarContrasenaPerf);
         btAnadriImpuestosPerfil = view.findViewById(R.id.anadirImpuestos);
         btCerrarSesionPerfil = view.findViewById(R.id.ibCerrarSesionPerfil);
-        btCerrarSesion = view.findViewById(R.id.btCerrarSesion);
 
         //Cargar datos del usuario si está autenticado
         if (usuarioActual != null) {
@@ -85,6 +171,18 @@ public class PerfilFragment extends Fragment {
         btVerTodoPerfil.setOnClickListener(v ->{
             Intent irADatosImpuestos = new Intent(getActivity(), informacionAdicional.class);
             startActivity(irADatosImpuestos);
+        });
+
+        // Manejar clic en la imagen de perfil para tomar foto
+        ivFotoPerfil.setOnClickListener(v ->{
+            //Intentamos abrir la cámara
+            if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED ||
+                    ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                abrirCamera();
+            } else {
+                ActivityCompat.requestPermissions(requireActivity(), new String[]{android.Manifest.permission.CAMERA}, REQUEST_PERMISSION);
+                System.out.println("No se puede abrir la camara, comprueba los permisos.");
+            }
         });
 
         //Botón cambiar contraseña
@@ -215,6 +313,23 @@ public class PerfilFragment extends Fragment {
                         //Si el valor es null, se muestra un mensaje predeterminado
                         etNombreApellidoPerfil.setText(nombre != null ? nombre : "Nombre no disponible");
                         etUsuarioPerfil.setText(email != null ? email : "Email no disponible");
+
+                        // Verificar si hay una imagen en Base64 subida por el usuario
+                        if (snapshot.child("photoBase64").exists()) {
+                            String imagenBase64 = snapshot.child("photoBase64").getValue(String.class);
+                            if (imagenBase64 != null && !imagenBase64.isEmpty()) {
+                                Log.d("PerfilFragment", "Base64 recuperada: " + imagenBase64);
+                                //Propio
+                                Bitmap imagenDecodificada = convertirBase64AImagen(imagenBase64);
+                                ivFotoPerfil.setImageBitmap(imagenDecodificada);
+                            } else {
+                                Log.e("PerfilFragment", "Imagen en Base64 está vacía o nula");
+                                //Google
+                                comprobarImagenGoogle();
+                            }
+                        } else {
+                            comprobarImagenGoogle();
+                        }
                     } else {
                         Toast.makeText(getActivity(), "No se encontraron datos", Toast.LENGTH_SHORT).show();
                     }
@@ -238,5 +353,66 @@ public class PerfilFragment extends Fragment {
         refSaldoInicial.setValue(Double.parseDouble(etSaldoInicialPerfil.getText().toString()));
         long currentTimeMillis = System.currentTimeMillis();
         refTiempoSaldoInicial.setValue(currentTimeMillis);
+    }
+
+    //Si hay aplicación de cámara
+    private void abrirCamera() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (cameraIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            Log.d("PerfilFragment", "Cámara encontrada, abriendo...");
+            cameraLauncher.launch(cameraIntent);
+        } else {
+            Log.e("PerfilFragment", "No se encontró una aplicación de cámara instalada.");
+            Toast.makeText(getActivity(), "No se encontró una aplicación de cámara", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //Si hay imagen de google
+    private void comprobarImagenGoogle() {
+        Uri Urlfoto = usuarioActual.getPhotoUrl();
+        if (Urlfoto != null) {
+            Glide.with(getContext())
+                    .load(Urlfoto)
+                    .into(ivFotoPerfil);
+        } else {
+            // Si no hay imagen en Google ni en Base64, poner la predeterminada
+            ivFotoPerfil.setImageResource(R.drawable.perfilimpuestos3);
+        }
+    }
+
+    //Captura y guarda la imagen de perfil en Firebase en formato Base64.
+    private void subirImagenAFirebase(Bitmap photo) {
+        String imagenBase64 = convertirImagenBase64(photo);
+
+        if (usuarioActual != null) {
+            String emailUser = usuarioActual.getEmail();
+            if (emailUser != null) {
+                String usuarioClave = emailUser.replace("@", "_").replace(".", "_");
+                DatabaseReference usuarioRef = database.getReference("Usuarios").child(usuarioClave);
+
+                usuarioRef.child("photoBase64").setValue(imagenBase64)
+                        .addOnSuccessListener(aVoid -> {
+                            Log.d("PerfilFragment", "Imagen guardada correctamente");
+                            Toast.makeText(getActivity(), "Imagen guardada", Toast.LENGTH_SHORT).show();
+                        })
+                        .addOnFailureListener(e -> {
+                            Log.e("PerfilFragment", "Error al guardar la imagen: " + e.getMessage());
+                            Toast.makeText(getActivity(), "Error al guardar la imagen", Toast.LENGTH_SHORT).show();
+                        });
+            }
+        }
+    }
+
+    //Para gardar
+    private String convertirImagenBase64(Bitmap bitmap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        return Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT);
+    }
+
+    //Para sacar
+    private Bitmap convertirBase64AImagen(String base64String) {
+        byte[] decodedBytes = Base64.decode(base64String, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
     }
 }
